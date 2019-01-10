43c74e7d37dcaaa7c21b58c33e06cadafd306a97
田久保 雄 <yu@zozo.com>
2019-01-10 18:56:49 +0900

    一旦コミット

---
+++ Keisan/Keisan/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Keisan
+//
+//  Created by yu on 2019/01/09.
+//  Copyright © 2019年 yu.takubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var count = 0
+    
+    var plus = 0
+    
+    var minus = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        count = 10
+        
+        count = 11
+        
+        count = count + 1
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
